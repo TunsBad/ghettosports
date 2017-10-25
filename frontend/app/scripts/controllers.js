baf5a5a2b9ad56ffd3513872842b6aedd16a2c16
TunsBad <badmus_inc@yahoo.com>
2017-10-25 14:17:40 +0000

    update v1.0.1

--- frontend/app/scripts/controllers.js
+++ frontend/app/scripts/controllers.js
@@ -164,7 +164,7 @@ angular.module('ghettoSports')
 
     marker.bindPopup("<b>Welcome !</b>");
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>', maxZoom: 18, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoidHVuZGViYWRtdXMiLCJhIjoiY2o5MDE0ODByMjdoNzMzcXVzYmppNDE0MCJ9.dxoVquDF9o3pPS1qtyrq_A'
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>', maxZoom: 16, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoidHVuZGViYWRtdXMiLCJhIjoiY2o5MDE0ODByMjdoNzMzcXVzYmppNDE0MCJ9.dxoVquDF9o3pPS1qtyrq_A'
     }).addTo(mymap);
 
     $scope.feedback = {
